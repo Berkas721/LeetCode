dc2a7d29f9602e457f9ba28b83c48ba84ee842b7
kasyanof.artem <kasyanof.artem2017@yandex.ru>
2024-06-30 02:27:38 +0300

    Added Startup and basic swagger

---
+++ src/LeetCode.Api/Startup/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Startup;
+
+public static partial class Startup
+{
+    public static Task<WebApplicationBuilder> ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddSwagger()
+            .AddControllers();
+
+        return Task.FromResult(builder);
+    }
+
+    public static async Task<WebApplication> Configure(this WebApplication app)
+    {
+        app
+            .UseDevelopmentConfiguration()
+            .UseHttpsRedirection();
+
+        return app;
+    }
+}
