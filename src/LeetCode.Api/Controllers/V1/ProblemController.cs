0c38288209c3150ec6203f39876bb02f26d31e95
kasyanof.artem <kasyanof.artem2017@yandex.ru>
2024-07-31 22:43:44 +0500

    Added ProblemController and Create request

---
+++ src/LeetCode.Api/Controllers/V1/ProblemController.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Controllers.Abstraction;
+using LeetCode.Dto.Problem;
+using LeetCode.Features.Problem.Create;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeetCode.Controllers.V1;
+
+[Route("api/v1/problem")]
+public class ProblemController(IMediator mediator, IMapper mapper) : ApplicationController(mediator, mapper)
+{
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateProblemInput input)
+    {
+        // TODO: вынести куда нибудь
+        var userId = User
+            .Claims
+            .First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")
+            .Value;
+
+        var command = Mapper.Map<CreateProblemCommand>(input) with { CreatorId = new Guid(userId) };
+        var problemId = await Mediator.Send(command);
+        return Ok(problemId);
+    }
+}
---
+++ src/LeetCode.Api/Features/Problem/Create/CreateProblemCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using LeetCode.Data.Contexts;
+using LeetCode.Data.Enums;
+using LeetCode.Data.OwnedTypes;
+using LeetCode.Exceptions;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeetCode.Features.Problem.Create;
+
+public sealed record CreateProblemCommand : IRequest<long>
+{
+    public required string Name { get; init; }
+
+    public required string Description { get; init; }
+
+    public required ProblemDifficulty Difficulty { get; init; } 
+
+    public required bool IsPremiumRequired { get; init; }
+
+    public required List<long> TopicIds { get; init; }
+
+    public required Guid CreatorId { get; init; }
+}
+
+public sealed record CreateProblemCommandHandler : IRequestHandler<CreateProblemCommand, long>
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    private readonly IMapper _mapper;
+
+    public CreateProblemCommandHandler(
+        ApplicationDbContext dbContext, 
+        IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<long> Handle(
+        CreateProblemCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var problem = _mapper.Map<Data.Entities.Problem>(request);
+
+        problem.CreateInfo = new ActionInfo
+        {
+            Date = DateTime.UtcNow,
+            AgentId = request.CreatorId
+        };
+
+        problem.Status = ProblemStatus.Draft;
+
+        var topics = await _dbContext
+            .ProblemTopics
+            .Where(x => request.TopicIds.Contains(x.Id))
+            .ToListAsync(cancellationToken);
+
+        if (topics.Count != request.TopicIds.Count)
+            throw new ResourceNotFoundException("некоторые topic не найдены");
+
+        problem.Topics = topics;
+
+        await _dbContext
+            .Problems
+            .AddAsync(problem, cancellationToken);
+
+        await _dbContext
+            .SaveChangesAsync(cancellationToken);
+
+        return problem.Id;
+    }
+}
